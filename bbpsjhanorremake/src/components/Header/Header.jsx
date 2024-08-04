90e0a38233d081904c63eedc1009ac0fcd9e8347
Shish Fruitwala <shishfruitwala29@gmail.com>
2024-08-04 12:16:43 +0530

    update header and home component

--- bbpsjhanorremake/src/components/Header/Header.jsx
+++ bbpsjhanorremake/src/components/Header/Header.jsx
@@ -4,64 +4,77 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 export default function Header() {
   const gsapLogoRef = useRef();
-
+  const tl = gsap.timeline();
   useGSAP(() => {
-    gsap.from(gsapLogoRef.current, {
+    tl.from(gsapLogoRef.current, {
       y: 30,
       duration: 1,
-      opacity: 0.2,
+      opacity: 0,
     });
   });
+
+  useGSAP(() => {
+    tl.from(".linkContainer .link", {
+      y: 150,
+      duration: 0.7,
+      stagger: 0.25,
+      opacity: 0,
+    });
+  });
+
   return (
     <header className="sticky top-0">
       <nav className="bg-[#08999a]">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" ref={gsapLogoRef}>
-            <img
-              src="https://bbpsjhanor.balbharati.org/wp-content/uploads/2017/01/jhanor.png"
-              alt="BBPS-Jhanor"
-            />
-          </Link>
-          <NavLink
-            to="/explore"
-            className={({ isActive }) => {
-              `${
-                isActive ? "text-white" : "text-red-500"
-              } md:text-lg text-white hover:text-black py-1 px-1`;
-            }}
-          >
-            Explore
-          </NavLink>
-          <NavLink
-            to="/gallery"
-            className={({ isActive }) => {
-              `${
-                isActive ? "text-white" : "text-red-500"
-              } md:text-lg text-white hover:text-black py-1 px-1`;
-            }}
-          >
-            Gallery
-          </NavLink>
-          <NavLink
-            to="/cbse-results"
-            className={({ isActive }) => {
-              `${
-                isActive ? "text-white" : "text-red-500"
-              } md:text-lg text-white hover:text-black py-1 px-1`;
-            }}
-          >
-            CBSE Results
-          </NavLink>
-          <Link
-            to="/magazine"
-            className={({ isActive }) => {
-              `${
-                isActive ? "text-white" : "text-red-500"
-              } md:text-lg text-white hover:text-black py-1 px-1`;
-            }}
-          >
-            Magazine
-          </Link>
+          <div ref={gsapLogoRef}>
+            <Link to="/">
+              <img
+                src="https://bbpsjhanor.balbharati.org/wp-content/uploads/2017/01/jhanor.png"
+                alt="BBPS-Jhanor"
+              />
+            </Link>
+          </div>
+
+          <div className="mr-8 linkContainer">
+            <NavLink
+              to="/explore"
+              className={({ isActive }) =>
+                `${
+                  isActive ? "text-black" : "text-white"
+                } md:text-lg text-white hover:text-black py-1 px-1 mx-12 link`
+              }
+            >
+              Explore
+            </NavLink>
+            <NavLink
+              to="/gallery"
+              className={({ isActive }) =>
+                `${
+                  isActive ? "text-black" : "text-white"
+                } md:text-lg text-white hover:text-black py-1 px-1  mx-12 link`
+              }
+            >
+              Gallery
+            </NavLink>
+            <NavLink
+              to="/cbse-results"
+              className={({ isActive }) =>
+                `${
+                  isActive ? "text-black" : "text-white"
+                } md:text-lg text-white hover:text-black py-1 px-1  mx-12 link`
+              }
+            >
+              CBSE Results
+            </NavLink>
+            <NavLink
+              to="/magazine"
+              className={({ isActive }) => `
+              ${isActive ? "text-black" : "text-white"}
+            } md:text-lg text-white hover:text-black py-1 px-1  mx-12 link`}
+            >
+              Magazine
+            </NavLink>
+          </div>
         </div>
       </nav>
     </header>
--- bbpsjhanorremake/src/components/Home/Home.jsx
+++ bbpsjhanorremake/src/components/Home/Home.jsx
@@ -1,4 +1,17 @@
 import React from "react";
 export default function Home() {
-  return "Home";
+  const [image, setImage] = useSate(
+    "https://bbpsjhanor.balbharati.org/wp-content/uploads/2024/07/IMG20240711101419.jpg"
+  );
+  return (
+    <>
+      <div className="flex justify-center">
+        <img
+          src="https://bbpsjhanor.balbharati.org/wp-content/uploads/2024/07/IMG20240711101419.jpg"
+          alt=""
+          className="object-fit bg-cover bg-no-repeat w-full"
+        />
+      </div>
+    </>
+  );
 }
